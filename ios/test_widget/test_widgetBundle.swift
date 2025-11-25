410ee21abe3dd783fd1d866a2dfc03163abf45a3
tohir-soft-tech-mac <mirxtohir@gmail.com>
2025-11-25 11:06:59 +0500

    widget added

---
+++ ios/test_widget/test_widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  test_widgetBundle.swift
+//  test_widget
+//
+//  Created by softtech on 25/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct test_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        test_widget()
+    }
+}
