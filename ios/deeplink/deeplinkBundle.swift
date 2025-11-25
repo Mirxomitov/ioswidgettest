5a45e8efa7858ca59ed886e2cf6b0abac0d7d13d
tohir-soft-tech-mac <mirxtohir@gmail.com>
2025-11-25 11:39:52 +0500

    deeplink widget added (version 17+)

---
+++ ios/deeplink/deeplinkBundle.swift
@@ -0,0 +1,16 @@
+//
+//  deeplinkBundle.swift
+//  deeplink
+//
+//  Created by softtech on 25/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct deeplinkBundle: WidgetBundle {
+    var body: some Widget {
+        deeplink()
+    }
+}
